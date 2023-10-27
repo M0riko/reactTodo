b844b1051934c2b7a28158b6924e4e42075f35ed
Andrey <adidenko753@gmail.com>
2023-10-27 15:19:22 +0300

    add new fix now

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -11,7 +11,7 @@ const Item = ({text, id, done, sesss, del, edit, edits, editText, onSetText, onS
                 <input type="text" id="change-text" onChange={(e) => onSetText(e)} defaultValue={text}/>
                 <button className="doneChange" onClick={editText}>EDIT</button>
                 </form>
-                    <div className={`task-text ${done && 'line'} ${edit === false ? '' : 'none'}`} onClick={onSetModal}>{text.length > 17 ? `${text.slice(0, 17)}...` : text}</div>
+                    <div className={`task-text ${done && 'line'} ${edit === false ? '' : 'none'}`} onClick={onSetModal}>{text.length > 15 ? `${text.slice(0, 15)}...` : text}</div>
                     <div className="task-buttons">
                     <button className="change" onClick={edits}><img src={change} alt="change"/></button>
                     <button className="done" onClick={sesss}><img src={dones} alt="change"/></button>
